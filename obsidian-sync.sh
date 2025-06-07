a7b6352ef142f1dab2c005ee4b962719bdd16744
theoradusz <theoradu@hotmail.fr>
2025-06-07 17:29:09 +0900

    deploy

--- obsidian-sync.sh
+++ obsidian-sync.sh
@@ -1,6 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-# copy new/updated files from obsidian to hugo 
-rsync -av "/Users/wota/obsidian/notes/5 blog/blog/" "/Users/wota/Documents/Perso/website/radusz/notes/content/blog/" 
+# copy only markdown files to content
+rsync -av "/Users/wota/obsidian/notes/5 blog/blog/" "/Users/wota/Documents/Perso/website/radusz/notes/content/blog/" --include="*.md" --exclude="*"
+
+# copy only images to static folder  
+rsync -av "/Users/wota/obsidian/notes/5 blog/blog/" "/Users/wota/Documents/Perso/website/radusz/notes/static/images/blog/" --include="*.png" --include="*.jpg" --include="*.jpeg" --include="*.gif" --include="*.webp" --exclude="*"
+
+find "/Users/wota/Documents/Perso/website/radusz/notes/content/blog/" -name "*.md" -exec sed -i '' 's|!\[\[\([^/][^]]*\)\]\]|![[\1]]|g; s|!\[\[\([^]]*\)\]\]|![](/images/blog/\1)|g' {} \;
+
 echo "blog files synced!"
-
